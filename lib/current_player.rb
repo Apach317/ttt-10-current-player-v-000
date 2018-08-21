e08c76f188c4a44e2614762fd55e1bfe4d748585
Ron Lynch <lynch.ronald1@gmail.com>
2018-08-21 18:55:03 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,15 @@
+def turn_count(board)
+  r = 0
+  counter = 0
+  while r <= 9
+    if board[r] == "X" || board[r] == "O"
+      counter += 1
+    end
+    r += 1
+  end
+  counter
+end
+
+def current_player(board)
+
+end
